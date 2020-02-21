a253310420f2006401abdfeb82c357736ec1878d
noothanprem <rutujatikhile@gmail.com>
2020-02-21 12:42:12 +0530

    added:player check for option

--- snake_ladder.sh
+++ snake_ladder.sh
@@ -11,3 +11,15 @@ function dieroll() {
 	dice=$(( RANDOM % 6 + 1 ))
 	echo $dice
 }
+
+#function to check for options in case of no play, ladder, or snake
+function optioncheck() {
+	options=$(( RANDOM % 3 + 1 ))
+	echo $options
+	case $options in 
+		1) current_pos=$current_pos;; #no play
+		2) current_pos=$(( $current_pos + $(dieroll) ));; # ladder
+		3) current_pos=$(( $current_pos - $(dieroll) ));; # snake
+	esac
+}
+
